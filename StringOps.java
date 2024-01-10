c3b2179067ed5312930f8e20b5b6b3361723194d
unknown <barbarbara.aberzon@post.runi.ac.il>
2024-01-10 21:05:00 +0200

    i love code

--- StringOps.java
+++ StringOps.java
@@ -1,42 +1,72 @@
+import java.util.Arrays;
+
 public class StringOps {
-    ////////////////////////////////////////////////////////////
-    //////                                               ///////
-    //////              Reminder:                        ///////
-    //////        allowed methods                        ///////
-    //////                                               ///////
-    //////        1.charAt(int index)                    ///////
-    //////        2.length()                             ///////
-    //////        3.substring(int start)                 ///////
-    //////        4.substring(int start,int ends)        ///////
-    //////        5.indexOf(String str)                  ///////
-    //////                                               ///////
-    //////        The rest are not allowed !             ///////
-    //////        if you want to use a different         ///////
-    //////        method, and you can implement          ///////
-    //////        it using material from the course      ///////
-    //////        you need to implement a version of     ///////
-    //////        the function by yourself.              ///////
-    //////                                               ///////
-    //////        see example for substring              ///////
-    //////        in Recitation 3 question 5             ///////
-    //////                                               ///////
-    ////////////////////////////////////////////////////////////
     public static void main(String[] args) {
-        
+        //tseter 
+        String word = "MMMM" ;
+        char letter = 'M';
+        System.out.println(Arrays.toString(allIndexOf(word, letter)));
+
+
     }
 
-    public static String capVowelsLowRest (String string) {
-        // Write your code here:
-        return "";
+    public static String capVowelsLowRest (String string) { 
+        String endString = "" ; 
+        String vowels = "aeiouAEIOU" ;
+        char c ;
+        for ( int i = 0 ; i < string.length() ; i++){
+                c = string.charAt(i) ;
+
+			if ( vowels.indexOf(c) == -1 &&  c <= 'Z' && c > 'A' ){
+                c = (char)(c+32);
+            }
+            
+            else if (vowels.indexOf(c) != -1 && c <= 'z' && c >= 'a' ){
+                c = (char)(c-32);
+            }
+         
+           endString = endString + c ;
+		}
+        return endString;
     }
 
     public static String camelCase (String string) {
-        // Write your code here:
-        return "";
+        String endString = "" ; 
+        char c = string.charAt(1) ; 
+
+        for (int i = 0 ; i < string.length() ; i++){
+            if ( c = 32 )
+                
+            }
+
+        for (int i = 0 ; i < ; i++){
+            if (string.charAt(i)
+        }
+        return endString;
     }
 
     public static int[] allIndexOf (String string, char chr) {
-        // Write your code here:
-        return new int[1];
+    int appears = 0 ;
+
+    //check how much time the letter appears
+    for (int i = 0 ; i < string.length() ; i++){
+        if (string.charAt(i) == chr){
+            appears++ ; 
+         }
+        }
+
+    //create new array
+    int [] newArray = new int[ appears ] ; 
+    int currentIndex = 0;
+
+   for (int i = 0 ; i < string.length() ; i ++){
+            if (string.charAt(i) == chr){
+            newArray [currentIndex] = i ; 
+            currentIndex++ ; 
+        } 
+    }
+    
+
+        return newArray;
     }
 }
